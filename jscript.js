35b7620c7214769f2e719015159d03b7363ed259
ekeminiwebdev <112984312+ekeminiwebdev@users.noreply.github.com>
2024-08-08 08:22:17 +0100

    Updated

--- jscript.js
+++ jscript.js
@@ -1,6 +1,6 @@
 let navLinks = document.getElementById('navLinks');
 function showMenu(){
-    navLinks.style.left = '20px';
+    navLinks.style.left = '30%';
 }
 function hideMenu(){
     navLinks.style.left = '-200px';
